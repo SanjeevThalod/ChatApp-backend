ca3e1efeb36701ee60251a8661bbdcae135bddaf
SanjeevThalod <thalodsanjeev@gmail.com>
2023-08-20 16:57:59 +0530

    update

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,16 +1,16 @@
-const notFound = async (req, res, next) => {
+const notFound = (req, res, next) => {
     const err = new Error(`Not Found ${req.url}`);
-    res.status(404);
+    err.status = 404; // Set the status property of the error object
     next(err);
 }
 
-const errorHandler = async (req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err.status || 500; // Use the status property if available
     res.status(statusCode);
     res.json({
         message: err.message,
         stack: process.env.NODE_ENV === "production" ? null : err.stack
-    })
+    });
 };
 
-module.exports = { notFound, errorHandler }
+module.exports = { notFound, errorHandler };
